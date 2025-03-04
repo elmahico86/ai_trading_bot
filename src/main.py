92df2f2e6d582d5f212d9253ebd251f82add57c3
elmahico86 <50416905+elmahico86@users.noreply.github.com>
2025-03-04 16:56:33 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,11 @@
-# src/main.py
+# main.py
 
-import asyncio
-from bot import TradingBot
-import logging
-import logging.config
-import json
+from src.bot import TradingBot
+from src.config import SYMBOL, TRADE_AMOUNT
 
-def setup_logging():
-    with open('logging_config.json', 'r') as f:
-        config_dict = json.load(f)
-    logging.config.dictConfig(config_dict)
+def main():
+    bot = TradingBot()
+    bot.run(SYMBOL, TRADE_AMOUNT)
 
 if __name__ == '__main__':
-    setup_logging()
-    bot = TradingBot()
-    try:
-        asyncio.run(bot.run())
-    except KeyboardInterrupt:
-        print("Bot interrotto dall'utente.")
-        asyncio.run(bot.stop())
+    main()
