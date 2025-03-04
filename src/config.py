42c42e1686f7decd5b6784ab966089f8d9ab950e
elmahico86 <50416905+elmahico86@users.noreply.github.com>
2025-03-04 17:28:22 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ API_SECRET = os.getenv('KUCOIN_API_SECRET')
 API_PASSPHRASE = os.getenv('KUCOIN_API_PASSPHRASE')
 
 # Impostazioni di trading
-PAPER_TRADING = True  # Modalità Paper Trading attiva per default
+PAPER_TRADING = True  # Modalità Paper Trading attiva di default
 
 # Parametri di trading
 SYMBOL = 'BTC-USDT'
