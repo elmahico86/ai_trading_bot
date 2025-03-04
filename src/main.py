d3e1500dd0309c564c0fa8415000ff1c069c94e6
elmahico86 <50416905+elmahico86@users.noreply.github.com>
2025-03-04 17:39:09 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 # main.py
 
 from src.bot import TradingBot
-from src.config import SYMBOL, TRADE_AMOUNT
 
 def main():
     bot = TradingBot()
-    bot.run(SYMBOL, TRADE_AMOUNT)
+    bot.run()
 
 if __name__ == '__main__':
     main()
